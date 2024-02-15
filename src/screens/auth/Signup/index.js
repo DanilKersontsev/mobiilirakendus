b7d48517878126bf37f592ace7948ad96f3446e8
Danil Kersontsev <danil.kersontsev@khk.ee>
2024-02-15 14:48:30 +0200

    AuthHeader and Input component

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,18 +1,13 @@
-import React from "react";
 import { View } from "react-native"
-import Input from "../../../components/Input";
 import AuthHeader from "../../../components/AuthHeader"
 import { styles } from "./styles"
+
 
 const Signup = () => {
     return (
         <View style={styles.container}>
-            <AuthHeader title="Sign up"></AuthHeader>
-            <Input label="Name" placeholder={"John Doe"}></Input>
-            <Input label="Email" placeholder="example@gmail.com"></Input>
-            <Input  isPassword label="Password" placeholder="******"></Input>
+            <AuthHeader title={"Sign up"}></AuthHeader>
         </View>
     )
 }
-
 export default Signup
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -2,6 +2,5 @@ export const colors = {
     blue: '#4F63AC',
     white: '#FFFFFF',
     orange: '#FCA34D',
-    black: '#000000',
-    grey: '#8D9BB5'
+    black: '#000000'
 }
