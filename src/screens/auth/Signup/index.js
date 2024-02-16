92334b061750b2739b3d96ea683c852b17bddb72
Danil Kersontsev <danil.kersontsev@khk.ee>
2024-02-16 11:15:32 +0200

    AuthHeader and Input component

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,13 +1,18 @@
-import { View } from "react-native"
-import AuthHeader from "../../../components/AuthHeader"
-import { styles } from "./styles"
+import React from "react";
+ import { View } from "react-native"
+ import Input from "../../../components/Input";
+ import AuthHeader from "../../../components/AuthHeader"
+ import { styles } from "./styles"
 
+ const Signup = () => {
+     return (
+         <View style={styles.container}>
+             <AuthHeader title="Sign up"></AuthHeader>
+             <Input label="Name" placeholder={"John Doe"}></Input>
+             <Input label="Email" placeholder="example@gmail.com"></Input>
+             <Input  isPassword label="Password" placeholder="******"></Input>
+         </View>
+     )
+ }
 
-const Signup = () => {
-    return (
-        <View style={styles.container}>
-            <AuthHeader title={"Sign up"}></AuthHeader>
-        </View>
-    )
-}
-export default Signup
+ export default Signup 
