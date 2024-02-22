fb33e46732080a44fbc851adcc92de53970a32ad
Danil Kersontsev <danil.kersontsev@khk.ee>
2024-02-22 13:24:16 +0200

    Filtreerimine kategooria järgi

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import { View, Text, FlatList } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../../components/Header";
@@ -7,29 +7,43 @@ import Header from "../../../components/Header";
  import CategoryBox from "../../../components/CategoryBox";
  import {products} from "../../../data/products"
  import ProductHomeItem from "../../../components/ProductHomeItem";
+ import { colors } from "../../../utils/colors";
 
 
  const Home = () => {
-     const renderCatergoryItem = (item,index) => {
-         return(
-             <CategoryBox title={item?.item?.title} image={item?.item?.image} />
-         )
-     }
+    const [selectedCategory, setSelectedCategory] = useState()
+    const [selectedProducts, setSelectedProducts] = useState(products)
 
-     const renderProductItem = ({item}) => {
-         console.log('item => ', item)
-         return(
-             <ProductHomeItem {...item} />
-         )
-     }
-     return(
-         <SafeAreaView>
-             <View style={styles.container}>
-                 <Header showSearch={true} title="Find All You Need"></Header>
-                 <FlatList showsHorizontalScrollIndicator={false} style={styles.list} horizontal data={categories} renderItem={renderCatergoryItem} keyExtractor={(item,index)=> String(index)} />
-                 <FlatList numColumns={2} data={products} renderItem={renderProductItem} keyExtractor={(item) => String(item.id)} ListFooterComponent={<View style={{height: 250}} />} />
-             </View>
-         </SafeAreaView>
-     )
+    useEffect(() => {
+        if(selectedCategory){
+            const updatedSelectedProducts = products.filter((product) =>
+            product?.category === selectedCategory)
+            setSelectedProducts(updatedSelectedProducts)
+        } else {
+            setSelectedProducts(products)
+        }
+    }, [selectedCategory])
+    const renderCatergoryItem = (item,index) => {
+        return(
+            <CategoryBox
+            onPress={() => setSelectedCategory(item?.item?.id)} isSelected={item.item.id === selectedCategory} title={item?.item?.title} image={item?.item?.image} />
+        )
+    }
+
+   const renderProductItem = ({item}) => {
+       console.log('item => ', item)
+       return(
+           <ProductHomeItem {...item} />
+       )
+   }
+   return(
+       <SafeAreaView>
+            <View style={styles.container}>
+                <Header showSearch={true} title="Find All You Need"></Header>
+                <FlatList showsHorizontalScrollIndicator={false} style={styles.list} horizontal data={categories} renderItem={renderCatergoryItem} keyExtractor={(item,index)=> String(index)} />
+                <FlatList numColumns={2} data={selectedProducts} renderItem={renderProductItem} keyExtractor={(item) => String(item.id)} ListFooterComponent={<View style={{height: 250}} />} />
+            </View>
+        </SafeAreaView>
+    )
 }
 export default React.memo(Home)
